7a0d8bcb9613c2e39bbafe40e0ac160fa163bbf3
Waterlilyrr <noemict94@hotmail.it>
2023-01-31 01:39:15 +0100

    feat: filter overlay logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import Slider from "./components/Slider";
 import Navbar from "./components/Navbar";
 import FilterList from "./components/FilterList";
 import { memo, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const { filterStatus } = useSelector((state) => state);
+  const dispatch = useDispatch();
   const [typeKey, setTypeKey] = useState("lip liner");
   const [brandKey, setBrandKey] = useState("");
+
+  const handleOverlayClick = () => {
+    dispatch({ type: "CLOSE_FILTER_MENU" });
+    dispatch({ type: "CLOSE_CATEGORY_LIST" });
+  };
 
   return (
     <div className={styles.App}>
       <Navbar />
       <Slider />
       <FilterList setBrandKey={setBrandKey} setTypeKey={setTypeKey} />
-
+      {(filterStatus.isFilterActive || filterStatus.isCategoryClicked) && (
+        <div className={styles.overlay} onClick={handleOverlayClick}></div>
+      )}
       {<MakeupList brandKey={brandKey} typeKey={typeKey} />}
       {/* {console.log("APP!!!")} */}
     </div>
