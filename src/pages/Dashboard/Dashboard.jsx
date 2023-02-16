c132a3ee3aa941f31158a53bd6bd3a08f4caa849
Waterlilyrr <noemict94@hotmail.it>
2023-02-16 20:15:25 +0100

    feat: favourite list in dashboard added

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,9 +4,22 @@ import { useSelector, useDispatch } from "react-redux";
 import styles from "./index.module.scss";
 
 const Dashboard = ({ user }) => {
+  const dispatch = useDispatch();
+  const { makeupData } = useSelector((state) => state);
+
   return (
     <div className={styles.Dashboard}>
       <h1>Buonsalve {localStorage.getItem("username")}</h1>
+      <h3>La tua lista dei preferiti:</h3>
+      <div>
+        {makeupData.favourites.length ? (
+          makeupData?.favourites?.map((favourite, index) => (
+            <p key={index}>{favourite.name}</p>
+          ))
+        ) : (
+          <p>Lista vuota</p>
+        )}
+      </div>
     </div>
   );
 };
