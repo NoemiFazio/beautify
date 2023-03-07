5db5d72b6def2ead2a5e54ff0ecba5813777f9ea
Waterlilyrr <noemict94@hotmail.it>
2023-03-07 21:19:59 +0100

    feat: ScrollToTop.jsx logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Login from "./pages/Login";
 import Error from "./pages/Error";
 import SharedLayout from "./pages/SharedLayout";
 import ProtectedRoute from "./pages/ProtectedRoute";
+import ScrollToTop from "./utils/ScrollToTop";
 // import Navbar from "./components/Navbar";
 // import { useSelector, useDispatch } from "react-redux";
 
@@ -36,57 +37,60 @@ function App() {
   return (
     <BrowserRouter>
       {/* <Navbar /> */}
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route
-            index
-            element={
-              <Home
-                typeKey={typeKey}
-                setTypeKey={setTypeKey}
-                brandKey={brandKey}
-                setBrandKey={setBrandKey}
-                labelCategoryValue={labelCategoryValue}
-                setLabelCategoryValue={setLabelCategoryValue}
-                labelBrandValue={labelBrandValue}
-                setLabelBrandValue={setLabelBrandValue}
-              />
-            }
-          />
-          <Route path="cart" element={<Cart />} />
-          <Route path="products" element={<Products />} />
-          {/* <Route path='products' element={<SharedProductLayout/>}>
+      <>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route
+              index
+              element={
+                <Home
+                  typeKey={typeKey}
+                  setTypeKey={setTypeKey}
+                  brandKey={brandKey}
+                  setBrandKey={setBrandKey}
+                  labelCategoryValue={labelCategoryValue}
+                  setLabelCategoryValue={setLabelCategoryValue}
+                  labelBrandValue={labelBrandValue}
+                  setLabelBrandValue={setLabelBrandValue}
+                />
+              }
+            />
+            <Route path="cart" element={<Cart />} />
+            <Route path="products" element={<Products />} />
+            {/* <Route path='products' element={<SharedProductLayout/>}>
             <Route index element={<Products/>} />
             <Route path=':productId' element={<Singleroduct/>} />
           </Route> */}
-          {/* In verità per il route qui sotto si poteva fare il nesting come in riga 31, come ho scritto nelle 4 righe precedenti,  ma funziona anche così */}
-          <Route
-            path="products/:productId"
-            element={
-              <SingleProduct
-                typeKey={typeKey}
-                setTypeKey={setTypeKey}
-                brandKey={brandKey}
-                setBrandKey={setBrandKey}
-                setLabelCategoryValue={setLabelCategoryValue}
-                setLabelBrandValue={setLabelBrandValue}
-                labelCategoryValue={labelCategoryValue}
-                labelBrandValue={labelBrandValue}
-              />
-            }
-          />
-          <Route path="login" element={<Login setUser={setUser} />} />
-          <Route
-            path="dashboard"
-            element={
-              <ProtectedRoute user={user}>
-                <Dashboard user={user} />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
+            {/* In verità per il route qui sotto si poteva fare il nesting come in riga 31, come ho scritto nelle 4 righe precedenti,  ma funziona anche così */}
+            <Route
+              path="products/:productId"
+              element={
+                <SingleProduct
+                  typeKey={typeKey}
+                  setTypeKey={setTypeKey}
+                  brandKey={brandKey}
+                  setBrandKey={setBrandKey}
+                  setLabelCategoryValue={setLabelCategoryValue}
+                  setLabelBrandValue={setLabelBrandValue}
+                  labelCategoryValue={labelCategoryValue}
+                  labelBrandValue={labelBrandValue}
+                />
+              }
+            />
+            <Route path="login" element={<Login setUser={setUser} />} />
+            <Route
+              path="dashboard"
+              element={
+                <ProtectedRoute user={user}>
+                  <Dashboard user={user} />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </>
     </BrowserRouter>
   );
 }
