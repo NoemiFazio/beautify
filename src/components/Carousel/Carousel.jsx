a5add3c9b63c5d991cf541e44d9707dad99e7b4d
Waterlilyrr <noemict94@hotmail.it>
2023-03-28 13:33:37 +0200

    feat: aria attributes added in Carousel component

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,47 +1,26 @@
 import styles from "./index.module.scss";
 import { useRef, memo } from "react";
 import CarouselCard from "../CarouselCard";
-// import GET from "../../utils/GET/GET";
-import { useSelector, useDispatch } from "react-redux";
-// import { IMPORT_URL } from "../../utils/GET/URL";
 import ScrollBtn from "../ScrollBtn";
 
 const Carousel = ({ data, type }) => {
   const carouselRef = useRef(null);
-  const dispatch = useDispatch();
-  // const { cartData } = useSelector((state) => state);
-
-  // useEffect(() => {
-  //   GET(
-  //     IMPORT_URL.ACTIVITIES,
-  //     "?discounted=YES&limit=20",
-  //     dispatch,
-  //     "SET_DISCOUNT_LIST",
-  //     lang,
-  //     currency
-  //   );
-  // }, [dispatch, lang, currency]);
-  // function getOccurrence(array, value) {
-  //   return array.filter((v) => v === value).length;
-  // }
 
   return (
-    <div className={styles.Carousel}>
-      {/* <div className={styles.title}>  */}
-      {/* <h3>{type}</h3> */}
-
-      {/* </div> */}
+    <div
+      type={`${type} carousel`}
+      aria-label={`${type} carousel`}
+      aria-required="true"
+      name={`${type} carousel`}
+      className={styles.Carousel}
+    >
       {
         <div className={styles.title}>
           <h3>{type}</h3>
           <ScrollBtn itemRef={carouselRef} />
         </div>
       }
-      <div
-        // ref={miniCarouselRef}
-        className={styles.mainContentDiv}
-        ref={carouselRef}
-      >
+      <div className={styles.mainContentDiv} ref={carouselRef}>
         {type === "beautycase"
           ? data
               ?.filter((value, index, self) => self.indexOf(value) === index)
@@ -51,9 +30,6 @@ const Carousel = ({ data, type }) => {
           : data?.map((item, index) => (
               <CarouselCard key={index} cardData={item} type={type} />
             ))}
-        {/* {activities?.discountList?.data?.map((cardData, index) => (
-          <MiniCard cardData={cardData} key={index} />
-        ))} */}
       </div>
     </div>
   );
